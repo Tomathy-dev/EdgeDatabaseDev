85832a11510c51bfb88b10655a4dcfce1520d2b5
Tommathy <tomas.b.salgueiro@gmail.com>
2023-09-08 16:01:10 +0100

    First steps

---
+++ my-app/src/lib/components/MainNav.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="w-20 h-full flex flex-col items-center shadow-md">
+	<div class="my-4">
+		<Icon icon="material-symbols:database-outline" class="text-5xl" />
+	</div>
+	<div class="w-16 h-[2px] bg-secondary" />
+	<div
+		class="mt-3 p-3 flex justify-center items-center rounded-md border-4 border-secondary hover:bg-base-200 hover:cursor-pointer"
+	>
+		<Icon icon="material-symbols:dashboard" class="text-3xl" />
+	</div>
+	<div
+		class="mt-3 p-3 flex justify-center items-center rounded-md hover:bg-base-200 hover:cursor-pointer"
+	>
+		<Icon icon="material-symbols:account-balance" class="text-3xl" />
+	</div>
+	<div
+		class="mt-3 p-3 flex justify-center items-center rounded-md hover:bg-base-200 hover:cursor-pointer"
+	>
+		<Icon icon="material-symbols:list-alt" class="text-3xl" />
+	</div>
+</div>
---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+	import MainNav from '$lib/components/MainNav.svelte';
+</script>
+
+<div class="w-screen h-screen flex flex-row overflow-hidden">
+	<!--SideBar Left-->
+	<MainNav />
+	<slot />
+</div>
